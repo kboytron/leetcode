595af5fd52494a105128733abdf668d8ce155648
Karan Singh <109481619+kboytron@users.noreply.github.com>
2024-07-29 11:53:06 -0700

    Time: 24 ms (61.15%) | Memory: 12 MB (71.00%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,18 +1,33 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
+        if not matrix or not matrix[0]:
+            return False
+        
         m, n = len(matrix), len(matrix[0])
-
-        left = 0
-        right = m*n-1
-
+        
+        # Step 1: Binary search to find the potential row
+        top, bottom = 0, m - 1
+        while top <= bottom:
+            mid = (top + bottom) // 2
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
+                row = mid
+                break
+            elif target < matrix[mid][0]:
+                bottom = mid - 1
+            else:
+                top = mid + 1
+        else:
+            return False  # Target is not in the range of the matrix
+        
+        # Step 2: Binary search in the found row
+        left, right = 0, n - 1
         while left <= right:
             mid = (left + right) // 2
-            mid_value = matrix[mid // n][mid % n]
-
-            if mid_value == target:
+            if matrix[row][mid] == target:
                 return True
-            elif mid_value < target:
+            elif matrix[row][mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
+        
         return False
